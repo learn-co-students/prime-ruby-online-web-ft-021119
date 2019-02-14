4cf58786b688e003221e70d14d26f95134e4e606
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-14 18:26:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  last_num = num - 1
+  divisors = (2...last_num)
+  if num <=1
+    false 
+  else
+    divisors.all? do |denominator|
+      num % denominator != 0
+    end
+  end
+end
