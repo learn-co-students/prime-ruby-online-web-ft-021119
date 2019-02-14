634901e539b896e93e4d1af4e6547c8cb5ac9a91
Lizzie Chan <lchan217@gmail.com>
2019-02-14 16:25:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  range_tested = (2..integer-1)
+  array = range_tested.to_a 
+  if integer < 2 
+    false 
+  else 
+    array.all? do |n|
+      integer % n != 0 
+    end 
+  end 
+end 
